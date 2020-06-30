d47179f9a6cec4bf7c1a69c6d1dc16961676a7c9
Sanna <sasymer@gmail.com>
2020-06-30 23:45:42 +0000

    Work on getting results from table selection into url

--- src/main/java/com/google/sps/servlets/DataFromDatabase.java
+++ src/main/java/com/google/sps/servlets/DataFromDatabase.java
@@ -1,4 +1,3 @@
-
 package com.google.sps.servlets;
 
 import com.google.cloud.spanner.DatabaseClient;
@@ -7,27 +6,23 @@ import com.google.cloud.spanner.ResultSet;
 import com.google.cloud.spanner.Spanner;
 import com.google.cloud.spanner.SpannerOptions;
 import com.google.cloud.spanner.Statement;
-import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns HTML that contains tables in the example database. */
-@WebServlet("/tables-from-db")
-public class TablesFromDatabase extends HttpServlet {
+/** Servlet that returns HTML that contains content from the example database. */
+@WebServlet("/CHANGETHISSS")
+public class DataFromDatabase extends HttpServlet {
 
   DatabaseClient dbClient;
 
   public void init() {
     Spanner spanner = SpannerOptions.newBuilder().build().getService();
-
-    //TODO: change "example-db" to the database that Millennia passes in from the db selection
-    DatabaseId db = DatabaseId.of("play-user-data-beetle", "test-instance", "example-db"); //project id, args, args
+    DatabaseId db = DatabaseId.of("play-user-data-beetle", "test-instance", "example-db");
     this.dbClient = spanner.getDatabaseClient(db);
   }
 
@@ -35,32 +30,28 @@ public class TablesFromDatabase extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
 
+    List<String> tablesToQuery = new ArrayList<>();
+    //get the list of strings from some url
+
     try (ResultSet resultSet =
         dbClient
             .singleUse() // Execute a single read or query against Cloud Spanner.
-            .executeQuery(Statement.of("SELECT table_name FROM information_schema.tables WHERE table_catalog = '' and table_schema = ''"))) {
-      List<String> tables = new ArrayList<>();
+            .executeQuery(Statement.of("SELECT SingerId, FirstName, LastName FROM Singers"))) {
       while (resultSet.next()) {
-        tables.add(resultSet.getString(0));
+        response
+            .getWriter()
+            .println(
+                String.format(
+                    "<p>%d %s %s</p>",
+                    resultSet.getLong(0),
+                    resultSet.isNull(1) ? "" : resultSet.getString(1),
+                    resultSet.isNull(2) ? "" : resultSet.getString(2)));
       }
-      String json = new Gson().toJson(tables);
-      response.getWriter().println(json);
     }
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    System.out.println("here is a post request");
-    String selected = request.getParameter("table-select");
-    System.out.println("HEREEEE " + selected);
 
-  ///problem -- only getting the first one that i selected
-
-    Enumeration enumer = request.getParameterNames();
-    while (enumer.hasMoreElements()) {
-      System.out.println(enumer.nextElement());
-    }
-
-    response.sendRedirect("/select-tables.html");
   }
 }
--- src/main/webapp/select-tables.js
+++ src/main/webapp/select-tables.js
@@ -4,11 +4,19 @@ function getList() {
   .then(response => response.json())
   .then((list) => { //list is a json object representing the list of tables
     const tableListSpace = document.getElementById('table-list');
+    createForm();
     createSelectElement();
     for (let index = 0; index < list.length; index++) {
       addTableOption(list[index]);
     }
+    createSubmit();
   });
+}
+
+function createSubmit() {
+  const submit = document.createElement("input");
+  submit.setAttribute("type", "submit");
+  document.getElementById("table-form").appendChild(submit);
 }
 
 function addTableOption(text) {
@@ -21,6 +29,45 @@ function addTableOption(text) {
 function createSelectElement() {
   const select = document.createElement("SELECT");
   select.setAttribute("id", "table-select");
-  document.body.appendChild(select);
+  select.setAttribute("name", "table-select");
+  document.getElementById("table-form").appendChild(select);
   document.getElementById("table-select").multiple = true;
+}
+
+function createForm() {
+  const form = document.createElement("form");
+  form.setAttribute("id", "table-form");
+  form.setAttribute("action", "/tables-from-db");
+  form.setAttribute("method", "POST");
+  document.body.appendChild(form);
+}
+
+function getSelectValues(select) {
+  var result = [];
+  var options = select && select.options;
+  var opt;
+
+  for (var i=0, iLen=options.length; i<iLen; i++) {
+    opt = options[i];
+
+    if (opt.selected) {
+      result.push(opt.value || opt.text);
+    }
+  }
+  return result;
+}
+
+// Prints selected table names onto page
+// Need to do a post request (?) to put the selected tables into a url, this way they can be saved and passed into the next page
+// to actually show the data from the tables.
+// Need to figure out how to do this. Maybe create a url that has the selected tables.
+function printSelected() {
+  const select = document.getElementById("table-select");
+  const selectedSpace = document.getElementById("selected");
+  var result = getSelectValues(select);
+  for (let index = 0; index < result.length; index++) {
+    const selectedElement = document.createElement("p");
+    selectedElement.innerText = result[index];
+    selectedSpace.appendChild(selectedElement);
+  }
 }
