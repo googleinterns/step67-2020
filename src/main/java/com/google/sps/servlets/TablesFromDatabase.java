948fcc7c61ac17cf48acdd692d299e8b599cf7e2
Sanna <sasymer@gmail.com>
2020-06-30 17:09:45 +0000

    Start working on table selection component

---
+++ src/main/java/com/google/sps/servlets/TablesFromDatabase.java
@@ -0,0 +1,47 @@
+
+package com.google.sps.servlets;
+
+import com.google.cloud.spanner.DatabaseClient;
+import com.google.cloud.spanner.DatabaseId;
+import com.google.cloud.spanner.ResultSet;
+import com.google.cloud.spanner.Spanner;
+import com.google.cloud.spanner.SpannerOptions;
+import com.google.cloud.spanner.Statement;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet that returns HTML that contains tables in the example database. */
+@WebServlet("/tables-from-db")
+public class TablesFromDatabase extends HttpServlet {
+
+  DatabaseClient dbClient;
+
+  public void init() {
+    Spanner spanner = SpannerOptions.newBuilder().build().getService();
+    DatabaseId db = DatabaseId.of("play-user-data-beetle", "test-instance", "example-db"); //project id, args, args
+    this.dbClient = spanner.getDatabaseClient(db);
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html;");
+
+    try (ResultSet resultSet =
+        dbClient
+            .singleUse() // Execute a single read or query against Cloud Spanner.
+            .executeQuery(Statement.of("SELECT table_name FROM information_schema.tables WHERE table_catalog = '' and table_schema = ''"))) {
+      List<String> tables = new ArrayList<>();
+      while (resultSet.next()) {
+        tables.add(resultSet.getString(0));
+      }
+      String json = new Gson().toJson(tables);
+      response.getWriter().println(json);
+    }
+  }
+}
---
+++ src/main/webapp/select-tables.js
@@ -0,0 +1,34 @@
+function getList() {
+  console.log('get list')
+  fetch('/tables-from-db')
+  .then(response => response.json())
+  .then((list) => { //list is a json object representing the list of tables
+    console.log('here');
+    console.log(list);
+    const tableListSpace = document.getElementById('table-list');
+    createSelectElement();
+    for (let index = 0; index < list.length; index++) {
+      addTableOption(list[index]);
+      //tableListSpace.appendChild(createListElement(list[index]));
+    }
+  });
+}
+
+function createListElement(text) {
+  const listElement = document.createElement('li');
+  listElement.innerText = text;
+  return listElement;
+}
+
+function addTableOption(text) {
+  const option = document.createElement("option");
+  option.setAttribute("value", text);
+  option.appendChild(document.createTextNode(text));
+  document.getElementById("table-select").appendChild(option);
+}
+
+function createSelectElement() {
+  const select = document.createElement("SELECT");
+  select.setAttribute("id", "table-select");
+  document.body.appendChild(select);
+}
