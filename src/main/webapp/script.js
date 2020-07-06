a3344699edba7225e92ab4ca3308a2cc7a944d02
hilakey <mmich.perez@gmail.com>
2020-07-06 18:42:32 +0000

    Add temporary hardcoded db list. Add "loading.." text until list has been filled. Remove console.log() functions. Remove whitespace. Adjust formatting.

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,37 +1,24 @@
 
 function getDatabases(){
- 
-    fetch("/example").then(response => response.json()).then((list) => {
-      console.log(list);
+  fetch("/databases").then(response => response.json()).then((list) => {
+    let dropdown = document.getElementById('list-databases');
+    dropdown.options.remove(0);
+    let defaultOption = document.createElement('option');
+    defaultOption.text = 'Select a Database';
 
-      let dropdown = document.getElementById('list-databases');
-      dropdown.length = 0;
-      console.log("The length of the dropdown is : " + dropdown.length);
+    dropdown.add(defaultOption);
+    dropdown.selectedIndex = 0;
 
-      let defaultOption = document.createElement('option');
-      defaultOption.text = 'Select a Database';
-
-      dropdown.add(defaultOption);
-      dropdown.selectedIndex = 0;
-
-      let option;
+    let option;
     
-    	for (let i in list) {
-          option = document.createElement('option');
-      	  option.text = list[i];
-            console.log(option.text);
-      	  option.value = list[i];
-            console.log(option.value);
-            if(list[i] != null){
-                dropdown.add(option);
-                console.log(list[i] + "was added into the dropdown");
-            }else{
-                console.log(list[i] + "was  not added into the dropdown");
-            }
-      	  
-        }
-
-
-    });
+    for (let i in list) {
+      option = document.createElement('option');
+      option.text = list[i];
+      option.value = list[i];
+      if(list[i] != null){
+        dropdown.add(option);
+      } 
+    }
+  });
 }
 
