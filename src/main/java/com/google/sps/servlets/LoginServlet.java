4b7ed0cf7423b943df2cda19e5232637147f6fed
William Croslen <wdcroslen@miners.utep.edu>
2020-07-07 16:52:09 +0000

    target folder deleted, and minor fixes made from last PR

--- src/main/java/com/google/sps/servlets/LoginServlet.java
+++ src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -1,17 +1,3 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.google.sps.servlets;
 
 import com.google.appengine.api.datastore.DatastoreService;
@@ -36,18 +22,20 @@ public class LoginServlet extends HttpServlet {
     response.setContentType("text/html");
 
     UserService userService = UserServiceFactory.getUserService();
+
     if (!userService.isUserLoggedIn()) {
       String urlToRedirectToAfterUserLogsIn = "/login";
       String loginUrl = userService.createLoginURL(urlToRedirectToAfterUserLogsIn);
-      response.getWriter().println("<style> body {margin-left:auto; margin-right:auto; width:650px; background-color: lightblue;} #canvas{background-color: lightblue;}</style> <p>Hello stranger.</p>");
+      response.getWriter().println("<p>Hello stranger.</p>");
       response.getWriter().println("<p>Login <a href=\"" + loginUrl + "\">here</a>.</p>");
       return;
     }
+
     String logoutUrl = "https://google.com";
     String userEmail = userService.getCurrentUser().getEmail();
     String homePage = "/index.html";
     response.getWriter().println("<h1> You are Logged in as " + userEmail + "!<h1>");
     response.getWriter().println("<a href=\"" +homePage+ "\"><button>Home</button/></a>");
     response.getWriter().println("<a href=\"" + logoutUrl + "\"><button>Logout</button/></a>");
-    } 
+  }
 } 
--- src/main/java/com/google/sps/servlets/ReasonServlet.java
+++ src/main/java/com/google/sps/servlets/ReasonServlet.java
@@ -15,7 +15,6 @@ public final class ReasonServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Get the input from the form.
     String text = getParameter(request, "reason", "");
-    System.out.println(text);
     String[] words = text.split("\\s*,\\s*");
     response.setContentType("text/html;");
     response.getWriter().println(Arrays.toString(words));
@@ -33,17 +32,3 @@ public final class ReasonServlet extends HttpServlet {
     return value;
   } 
 }
-
-    // boolean upperCase = Boolean.parseBoolean(getParameter(request, "upper-case", "false"));
-    // for now just test getParam(request, database selection);
-    
-    // here is my idea
-    /* We use a switch statement for whatever table has been selected
-    to determine which State to be in for querying the data.*/
-
-    /* We can use this same principle for selecting the database,
-     after a database is chosen we use a switch statement to see the tables.
-     Maybe, I just need to figure out how to not hardcode the tables */
-
-    // Break the text into individual words.
-    //get list of play user data team and check if the current user is one of them
